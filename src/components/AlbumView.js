72610aa091612c9f75393cfbf326ff1e3d8d3cd5
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-10-09 20:57:34 -0500

    fetch data album

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,12 +1,33 @@
 
 import { useState, useEffect } from 'react'
+import { useParams} from 'react-router-dom'
 
 function AlbumView() {
+    const {id} = useParams()
     const [ albumData, setAlbumData ] = useState([])
 
+useEffect(()=> {
+    const API_URL = `http://localhost:4000/song/${id}`
+    const fetchData = async () => {
+        const responce = await fetch(API_URL)
+        const data = await responce.json()
+        console.log(data)
+        const albumData = data.results.filter(item => item.wrapperType === 'track')
+        setAlbumData(albumData)
+    }
+    fetchData()
+}, [id])
+
+        const albumDataDisplay = albumData.map(track =>{
+            return(
+                <div key={track.trackId}>
+                    <p>{track.trackName}</p>
+                </div>
+            )
+        })
     return (
         <div>
-            <p>Album Data Goes Here!</p>
+            {albumDataDisplay}
         </div>
     )
 }
