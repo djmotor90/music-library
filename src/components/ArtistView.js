3f48bb251d65f44eb3cc20bf74fec52f3d50a0fd
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-10-11 20:05:52 -0500

    1

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,5 +1,4 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about our artist
+
 import { useState, useEffect } from 'react'
 import { useParams, Link, useNavigate} from 'react-router-dom'
 
